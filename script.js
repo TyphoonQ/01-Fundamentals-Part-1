488a6584785a8337e7de4e50fad96899e6e953e0
Tyler Quintana <tylerquintana537@gmail.com>
2025-07-15 12:56:39 -0600

    Type Conversion and Coercion Assignment

--- script.js
+++ script.js
@@ -289,7 +289,7 @@ if (BMIMark > BMIJohn) {
 } else {
     console.log(`John's BMI ${BMIJohn} is higher than Mark's ${BMIMark}!`);
 }
-*/
+
 
 // 20
 // type conversion
@@ -310,3 +310,10 @@ console.log('23' / '2');
 let n = '1' + 1; // '11'
 n = n - 1;
 console.log(n);
+*/
+
+console.log('9' - '5'); // -> 4
+console.log('19' - '13' + '17'); // -> 23
+console.log('19' - '13' + 17); // -> 617
+console.log('123' < 57); // -> false
+console.log(5 + 6 + '4' + 9 - 4 - 2); // -> 1143

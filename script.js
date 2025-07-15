dc5d662a77d5234573cf6c2cf201aaa3c686d79d
Tyler Quintana <tylerquintana537@gmail.com>
2025-07-15 12:52:45 -0600

    Type Conversion and Coercion

--- script.js
+++ script.js
@@ -262,7 +262,6 @@ if (population > 33) {
         `${country}'s population is ${33 - population} millions below average`
     );
 }
-*/
 
 
 // Coding Challenge #2
@@ -283,10 +282,31 @@ const BMIMark = massMark / (heightMark * heightMark);
 const BMIJohn = massJohn / (heightJohn * heightJohn);
 console.log(BMIMark, BMIJohn);
 
-/* Write your code below. Good luck! 🙂 */
+Write your code below. Good luck! 🙂 
 
 if (BMIMark > BMIJohn) {
     console.log(`Mark's BMI ${BMIMark} is higher than John's ${BMIJohn}!`);
 } else {
     console.log(`John's BMI ${BMIJohn} is higher than Mark's ${BMIMark}!`);
 }
+*/
+
+// 20
+// type conversion
+const inputYear = '1991';
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number('Jonas'));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+//type coercion
+console.log('I am ' + 23 + ' years old')
+console.log('23' - '10' - 3);
+console.log('23' / '2');
+
+let n = '1' + 1; // '11'
+n = n - 1;
+console.log(n);

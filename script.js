cc48d184a7f32d337c4839c56d146df1e03ee4ce
Tyler Quintana <tylerquintana537@gmail.com>
2025-07-16 16:24:27 -0600

    logical operators

--- script.js
+++ script.js
@@ -371,7 +371,7 @@ if (favourite === 23) {
 }
 
 if (favourite !== 23) console.log('Why not 23?')
-*/
+
 
 const numNeighbours = prompt('How many neighbour countries does your country have?');
 console.log(numNeighbours);
@@ -387,4 +387,31 @@ if (numNeighbours === 1) {
     console.log('More than 1 border')
 } else {
     console.log('No borders')
+}
+*/
+
+// 24
+
+const hasDriversLicense = true; // A
+const hasGoodVision = true; // B
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+//const shouldDrive = hasDriversLicense && hasGoodVision;
+
+// if (hasDriversLicense && hasGoodVision) {
+//     console.log('Sarah is able to drive!');
+// } else {
+//     console.log('Someone else should drive...');
+// }
+
+const isTired = false; // C
+console.log(hasDriversLicense && hasGoodVision && isTired);
+
+if (hasDriversLicense && hasGoodVision && !isTired) {
+    console.log('Sarah is able to drive!');
+} else {
+    console.log('Someone else should drive...');
 }
